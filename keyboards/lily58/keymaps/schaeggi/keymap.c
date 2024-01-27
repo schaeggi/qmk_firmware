25fcdcaf2ae65ee512d7ee15b5b29de87cbd23e1
Timo <schaeggi@posteo.de>
2024-01-27 21:03:25 +0100

    Lily58 layout improvements

--- keyboards/lily58/keymaps/schaeggi/keymap.c
+++ keyboards/lily58/keymaps/schaeggi/keymap.c
@@ -161,9 +161,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 /* Layer 2 - Control and movement
  * ,----------------------------------------.                    ,-----------------------------------------.
- * |  ESC |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  |  F10 |  Bsp |
+ * |(ESK)*|  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  |  F10 |  Bsp |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | (CC)*|  Ins | Home |  Up  |  End | PgUp |                    |   +  |   7  |   8  |   9  |  F11 |  Del |
+ * | (TM)*|  Ins | Home |  Up  |  End | PgUp |                    |   +  |   7  |   8  |   9  |  F11 |  Del |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | LSft |  Del | Right| Down | Left | PgDn |-------.    ,-------|   -  |   4  |   5  |   6  |  F12 | Enter|
  * |------+------+------+------+------+------| KLock |    |NumLock|------+------+------+------+------+------|
@@ -173,34 +173,34 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                   |      |      |      |/       /         \+LSft \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  *
- *  *(EX) ... LAlt + F4
- *  *(CC) ... LCtl + #
+ *  *(ESK) ... Escape / LAlt + F4 Double tap
+ *  *(TM) ... LCtl + LAlt + Del  LCA(KC_DEL)
  */
 
 [2] = LAYOUT(
-  CC_ESK,            KC_F1,    KC_F2,      KC_F3,      KC_F4,      KC_F5,                             KC_F6,       KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_TRNS,
-  LCTL(KC_NUHS),     KC_INS,   KC_HOME,    KC_UP,      KC_END,     KC_PGUP,                           KC_KP_PLUS,  KC_KP_7, KC_KP_8, KC_KP_9, KC_F11,   KC_TRNS,
-  CC_STC,            KC_DEL,   KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_PGDN,                           KC_KP_MINUS, KC_KP_4, KC_KP_5, KC_KP_6, KC_F12,   KC_TRNS,
-  KC_TRNS,           KC_PSCR,  LCTL(KC_X), LCTL(KC_A), LCTL(KC_C), LCTL(KC_V),  QK_LOCK,    KC_NUM,   KC_KP_0,     KC_KP_1, KC_KP_2, KC_KP_3, KC_COMMA,  KC_TRNS,
+  CC_ESK,        KC_F1,    KC_F2,      KC_F3,      KC_F4,      KC_F5,                             KC_F6,       KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_TRNS,
+  LCA(KC_DEL),   KC_INS,   KC_HOME,    KC_UP,      KC_END,     KC_PGUP,                           KC_KP_PLUS,  KC_KP_7, KC_KP_8, KC_KP_9, KC_F11,   KC_TRNS,
+  CC_STC,        KC_DEL,   KC_LEFT,    KC_DOWN,    KC_RGHT,    KC_PGDN,                           KC_KP_MINUS, KC_KP_4, KC_KP_5, KC_KP_6, KC_F12,   KC_TRNS,
+  KC_TRNS,       KC_PSCR,  LCTL(KC_X), LCTL(KC_A), LCTL(KC_C), LCTL(KC_V),  QK_LOCK,    KC_NUM,   KC_KP_0,     KC_KP_1, KC_KP_2, KC_KP_3, KC_COMMA,  KC_TRNS,
                                            KC_TRNS,    KC_TRNS,    KC_TRNS,     KC_TRNS,    KC_TRNS,  KC_KP_0,     KC_KP_ASTERISK, KC_KP_SLASH
 ),
 
 /* Layer 3 - Shortcuts, Custom config
  * ,-----------------------------------------.                    ,-----------------------------------------.
- * | (EX)*| GoD1 | GoD2 | GoD3 |  MtP |  MtN |                    |      |      |      |      |      | Reset|
+ * | SC_Q | GoD1 | GoD2 | GoD3 |  MtP |  MtN |                    |      |      |      |      |      | Reset|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | (TM)*| SC_Q | Mail |FileMn| SC_P | SC_B |                    | LMB  | RMB  |      |      |      |      |
+ * | (CC)*| SC_Q | Mail |FileMn| SC_P | SC_B |                    | LMB  | RMB  |      |      |      |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | (V+)*| SC_A | SC_R | SC_S | SC_T | SC_G |-------.    ,-------|      |      |      |      |      | SC_Q |
+ * | (V+)*| SC_A | SC_R | SC_S | SC_T | SC_G |-------.    ,-------| MMB  |      |      |      |      | SC_Q |
  * |------+------+------+------+------+------|  NVD  |    |  PVD  |------+------+------+------+------+------|
- * | (V-)*|MsBtn3| SC_X |  Web | Calc | SC_V |-------|    |-------|      |      |      |      |      |      |
+ * | (V-)*|      | SC_X |  Web | Calc | SC_V |-------|    |-------|      |      |      |      |      |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | (MP)*| Layer3|Layer2| /Space  /       \Enter \  |      |     |      |
  *                   |      |      |      |/       /         \+LSft \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  *
- *  *(EX) ... LAlt + F4
- *  *(TM) ... LCtl + LAlt + Del  LCA(KC_DEL)
+ *  
+ *  *(CC) ... LCtl + # (VSCode toggle comment)
  *  *GoDn ... Go to Desktop n (xfce only)
  *  *MtP/MtN ... Move active window to Previous/Next desktop (xfce only)
  *  *(MP) ... Media play/pause
@@ -210,11 +210,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
 [3] = LAYOUT(
-  LALT(KC_F4),  LCTL(KC_F1),  LCTL(KC_F2), LCTL(KC_F3),  LCA(KC_HOME),  LCA(KC_END),                         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  CC_DRST,
-  LCA(KC_DEL),  MEH(KC_Q),    KC_MAIL,     KC_MYCM,      MEH(KC_P),     MEH(KC_B),                           KC_BTN1, KC_BTN2, KC_NO,   KC_NO,   KC_NO,  KC_NO,
-  KC_VOLU,      MEH(KC_A),    MEH(KC_R),   MEH(KC_S),    MEH(KC_T),     MEH(KC_G),                           KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  MEH(KC_Q),
-  KC_VOLD,      KC_BTN3,      MEH(KC_X),   KC_WHOM,      KC_CALC,       MEH(KC_V),    CC_WPDT,    CC_WNDT,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  DF(4),
-                                           KC_MPLY,      KC_TRNS,       KC_TRNS,      KC_TRNS,    KC_TRNS,   KC_NO,   KC_NO,   KC_NO
+  MEH(KC_Q),      LCTL(KC_F1),  LCTL(KC_F2), LCTL(KC_F3),  LCA(KC_HOME),  LCA(KC_END),                         KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  CC_DRST,
+  LCTL(KC_NUHS),  MEH(KC_Q),    KC_MAIL,     KC_MYCM,      MEH(KC_P),     MEH(KC_B),                           KC_BTN1, KC_BTN2, KC_NO,   KC_NO,   KC_NO,  KC_NO,
+  KC_VOLU,        MEH(KC_A),    MEH(KC_R),   MEH(KC_S),    MEH(KC_T),     MEH(KC_G),                           KC_BTN3, KC_NO,   KC_NO,   KC_NO,   KC_NO,  MEH(KC_Q),
+  KC_VOLD,        KC_NO,      MEH(KC_X),     KC_WHOM,      KC_CALC,       MEH(KC_V),    CC_WPDT,    CC_WNDT,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,  DF(4),
+                                             KC_MPLY,      KC_TRNS,       KC_TRNS,      KC_TRNS,    KC_TRNS,   KC_NO,   KC_NO,   KC_NO
   ),
 
 /* Layer 4 - Gaming Layer
@@ -225,7 +225,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |   G  | LSft |   A  |   S  |   D  |   F  |-------.    ,-------|   M  | Left | Down | Right|   O  | Enter|
  * |------+------+------+------+------+------| Enter |    |       |------+------+------+------+------+------|
- * |   T  | LCtl |   Y  |   X  |   C  |   V  |-------|    |-------|   K  |   H  |   G  |   T  |   /  | Lay0*|
+ * |   T  | LCtl |   Y  |   X  |   C  |   V  |-------|    |-------|   K  |   H  |   B  |   T  |   /  | Lay0*|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LAlt |  MMB  | LAlt| / Space /       \ RSft \  |      |      |      |
  *                   |      |      |      |/       /         \      \ |      |      |      |
@@ -237,7 +237,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC, KC_1,    KC_2, KC_3,    KC_4,    KC_5,                           KC_6,   KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_TAB, KC_I,    KC_Q, KC_W,    KC_E,    KC_R,                           KC_J,   KC_L,    KC_UP,   KC_Z,    KC_NUHS, KC_DEL,
   KC_G,   KC_LSFT, KC_A, KC_S,    KC_D,    KC_F,                           KC_M,   KC_LEFT, KC_DOWN, KC_RGHT, KC_O,    KC_ENT,
-  KC_T,   KC_LCTL, KC_Y, KC_X,    KC_C,    KC_V,    KC_ENT,       DF(0),   KC_K,   KC_H,    KC_G,    KC_T,    KC_PSLS, DF(0),
+  KC_T,   KC_LCTL, KC_Y, KC_X,    KC_C,    KC_V,    KC_ENT,       DF(0),   KC_K,   KC_H,    KC_B,    KC_T,    KC_PSLS, DF(0),
                          KC_LALT, KC_BTN3, KC_LALT, KC_SPC,       KC_RSFT, KC_NO,  KC_NO,   KC_NO
   )
 };
