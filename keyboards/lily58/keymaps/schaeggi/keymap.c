5570392aa2bd130ef0e3e021ee64ebda2364fbcb
Timo <schaeggi@posteo.de>
2024-06-30 13:57:34 +0200

    changed lily 58 combos for better usability. maybe

--- keyboards/lily58/keymaps/schaeggi/keymap.c
+++ keyboards/lily58/keymaps/schaeggi/keymap.c
@@ -6,20 +6,32 @@
 // Combos - multiple keys pressed at the same time
 const uint16_t PROGMEM combo_fp[] = {KC_F, KC_P, COMBO_END};
 const uint16_t PROGMEM combo_wf[] = {KC_W, KC_F, COMBO_END};
+
 const uint16_t PROGMEM combo_st[] = {KC_S, KC_T, COMBO_END};
-// const uint16_t PROGMEM combo_rs[] = {KC_R, KC_S, COMBO_END};
-const uint16_t PROGMEM combo_ne[] = {KC_N, KC_E, COMBO_END};
-// const uint16_t PROGMEM combo_ei[] = {KC_E, KC_I, COMBO_END};
+const uint16_t PROGMEM combo_rt[] = {KC_R, KC_T, COMBO_END};
+const uint16_t PROGMEM combo_tg[] = {KC_T, KC_G, COMBO_END};
+
 const uint16_t PROGMEM combo_lu[] = {KC_L, KC_U, COMBO_END};
 const uint16_t PROGMEM combo_uz[] = {KC_U, KC_Y, COMBO_END};
+
+const uint16_t PROGMEM combo_ne[] = {KC_N, KC_E, COMBO_END};
+const uint16_t PROGMEM combo_ni[] = {KC_N, KC_I, COMBO_END};
+const uint16_t PROGMEM combo_mn[] = {KC_M, KC_N, COMBO_END};
 
 combo_t key_combos[] = {
     COMBO(combo_fp, LCTL(KC_DEL)),
     COMBO(combo_wf, LCTL(KC_BSPC)),
-    COMBO(combo_st, RSFT(KC_8)),
-    COMBO(combo_ne, RSFT(KC_9)),
+    
+    COMBO(combo_st, KC_LEFT),
+    COMBO(combo_rt, LCTL(KC_LEFT)),
+    COMBO(combo_tg, RSFT(KC_8)),
+    
     COMBO(combo_lu, LCTL(KC_LEFT)),
     COMBO(combo_uz, LCTL(KC_RIGHT)),
+
+    COMBO(combo_ne, KC_RIGHT),
+    COMBO(combo_ni, LCTL(KC_RIGHT)),
+    COMBO(combo_mn, RSFT(KC_9)),
 };
 
 typedef enum {
